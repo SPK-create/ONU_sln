c0790ffdbf610097da298c82162ebaa40075f158
Reddi Biggs <bugenkor@gmail.com>
2025-05-11 15:42:20 +0200

    Reverse Polish Notation done

--- ONU/1-st course 2-d semester/12. Stacks usage/Reverse Polish Notation.cpp
+++ ONU/1-st course 2-d semester/12. Stacks usage/Reverse Polish Notation.cpp
@@ -1,15 +1,82 @@
 #include <iostream>
+#include <vector>
 #include <stack>
 using namespace std;
 
+int toINT(string&);
+void calculate(const string&, stack<int>&);
+
 int main() {
-	string str;
-	cin >> str;
-
 	stack<int> numbers;
-	stack<char> operand;
+	vector<char> operands;
 
-	//no solution yet(
-	
+	string str;
 
+	while (cin >> str) {
+		if (str == "+" || str == "-" || str == "*" || str == "/") {
+			//cout << "Operand" << endl;
+			calculate(str, numbers);
+		}
+		else {
+			//cout << "Number" << endl;
+			//Запишем число в стек, преобразуя из формата строки в целое число
+			//cout << toINT(str) << endl;
+
+			int number = toINT(str);
+			numbers.push(number);
+
+		}
+	}
+
+	cout << numbers.top();
+
+	/*while (!numbers.empty()) {
+		cout << numbers.top() << endl;
+		numbers.pop();
+	}*/
+
+	return 0;
+
+}
+
+int toINT(string& s) {
+	int length = s.size();
+	int number = 0;
+	for (int i = 0; i < s.size(); i++) { //Example: 158 = 1*10^2 + 5*10^1 + 8*10^0
+		int digit = s[i] - '0'; //Перевод из строки в целое число, не изменяя цифру
+		for (int j = 0; j < (length - 1); j++) {
+			digit *= 10;
+		}
+		length--;
+		number += digit;
+	}
+	return number;
+}
+
+void calculate(const string& str, stack<int>& numbers) {
+
+	if (numbers.size() > 1) {
+		int b = numbers.top();
+		numbers.pop();
+		int a = numbers.top();
+		numbers.pop();
+
+		if (str == "+") {
+			numbers.push(a + b);
+		}
+		if (str == "-") {
+			numbers.push(a - b);
+		}
+		if (str == "*") {
+			numbers.push(a * b);
+		}
+		if (str == "/") {
+			numbers.push(a / b);
+		}
+	}
+	else {
+		cout << "Operation with only one number is not declared!" << endl;
+	}
+
+	return;
 }
